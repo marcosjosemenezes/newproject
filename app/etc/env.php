d3c9767e02ace021df8ab2f2430455a164c174c1
Marcos J M Santos <marcosjosemenezes@bol.com.br>
2019-10-07 19:02:03 -0300

    New Tag V2.0.0

---
+++ app/etc/env.php
@@ -0,0 +1,51 @@
+<?php
+return [
+    'backend' => [
+        'frontName' => 'admin_1fiaoa'
+    ],
+    'crypt' => [
+        'key' => '1735f4e4505e1ccd53f719492d666836'
+    ],
+    'db' => [
+        'table_prefix' => '',
+        'connection' => [
+            'default' => [
+                'host' => 'localhost',
+                'dbname' => 'magento',
+                'username' => 'root',
+                'password' => '',
+                'active' => '1'
+            ]
+        ]
+    ],
+    'resource' => [
+        'default_setup' => [
+            'connection' => 'default'
+        ]
+    ],
+    'x-frame-options' => 'SAMEORIGIN',
+    'MAGE_MODE' => 'default',
+    'session' => [
+        'save' => 'files'
+    ],
+    'cache_types' => [
+        'config' => 1,
+        'layout' => 1,
+        'block_html' => 1,
+        'collections' => 1,
+        'reflection' => 1,
+        'db_ddl' => 1,
+        'compiled_config' => 1,
+        'eav' => 1,
+        'customer_notification' => 1,
+        'config_integration' => 1,
+        'config_integration_api' => 1,
+        'full_page' => 1,
+        'config_webservice' => 1,
+        'translate' => 1,
+        'vertex' => 1
+    ],
+    'install' => [
+        'date' => 'Wed, 13 Feb 2019 17:56:47 +0000'
+    ]
+];
